0afd6641f95dbc55fb30983f0300b3b170e2adb0
Douglas Felipe <117777160+DougFelipe@users.noreply.github.com>
2024-01-30 09:53:21 -0300

    feat: Avaliação 3 da disciplina

---
+++ IMD0404 - DESENVOLVIMENTO DE SISTEMAS WEB I/AV3/main.js
@@ -0,0 +1,19 @@
+
+// PÁGINA DE LOGUIN
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if(username === 'admin' && password === 'admin') {
+        window.location.href = 'venda.html'; // Redireciona para a página de cadastro
+    } else {
+        alert('Login e/ou senha incorretos');
+    }
+});
+
+document.getElementById('limpar').addEventListener('click', function() {
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+});
