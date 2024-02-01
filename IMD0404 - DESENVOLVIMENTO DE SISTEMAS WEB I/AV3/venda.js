3385a9abd94a664389a5188fc10a6aefa376049b
Matheus Queiroz <137003929+matheusqap@users.noreply.github.com>
2024-02-01 01:07:31 -0300

    venda.js atualizado

--- IMD0404 - DESENVOLVIMENTO DE SISTEMAS WEB I/AV3/venda.js
+++ IMD0404 - DESENVOLVIMENTO DE SISTEMAS WEB I/AV3/venda.js
@@ -1,7 +1,5 @@
-class Passageiro
-{
-    constructor(nome, cpf, endereco, classe)
-    {
+class Passageiro {
+    constructor(nome, cpf, endereco, classe) {
         this.nome = nome;
         this.cpf = cpf;
         this.endereco = endereco;
@@ -11,20 +9,54 @@ class Passageiro
 
 var passageiros = [];
 
-function cadastrarPassageiro()
-{
-    var nome = document.getElementById('nome').value;
-    var cpf = document.getElementById('cpf').value;
-    var endereco = document.getElementById('endereco').value;
-    
-    var opcoes = document.getElementsByName('opcao');
-    var classe;
+function verificarInputs() {
 
-    for (var i = 0; i < opcoes.length; i++) {
-        if (opcoes[i].checked) {
-            classe = opcoes[i].value;
+    try {
+
+        var nome = document.getElementById('nome').value;
+        var cpf = document.getElementById('cpf').value;
+        var endereco = document.getElementById('endereco').value;
+        var opcoes = document.getElementsByName('opcao');
+        var opcao;
+
+        for (var i = 0; i < opcoes.length; i++) {
+            if (opcoes[i].checked == true) {
+                opcao = opcoes[i].value;
+                break;
+            }
+            if (opcoes[opcoes.length - 1].checked == false) {
+                opcao = '';
+            }
         }
+
+        if (nome === '' || cpf === '' || endereco === '' || opcao === '') {
+            throw new Error('Preencha todos os inputs.');
+        }
+
+        else {
+
+            for (pessoa of passageiros) {
+                if (pessoa.cpf == cpf) {
+                    window.alert("CPF já cadastrado!");
+                    return;
+                }
+            }
+
+
+            cadastrarPassageiro(nome, cpf, endereco, opcao);
+        }
+
+
+    } catch (error) {
+        window.alert(error.message);
+
     }
+
+
+
+}
+
+function cadastrarPassageiro(nome, cpf, endereco, classe) {
 
     var passageiro = new Passageiro(nome, cpf, endereco, classe);
     passageiros.push(passageiro);
@@ -33,17 +65,21 @@ function cadastrarPassageiro()
     document.getElementById('nome').value = '';
     document.getElementById('cpf').value = '';
     document.getElementById('endereco').value = '';
-    var opcoes = document.getElementsByName('opcao');
-    for (var i = 0; i < opcoes.length; i++) {
-        opcoes[i].checked = false;
+    var classe = document.getElementsByName('opcao');
+    for (var i = 0; i < classe.length; i++) {
+        classe[i].checked = false;
     }
 
     //  alerta de cadastro realizado
-    alert('Cadastro Realizado');
+    alert('Cadastro Realizado com sucesso!');
 
-    // Teste para ver se está cadastrando os passageiros no vetor
-    for(var pessoa of passageiros)
-    {
+    /*// Teste para ver se está cadastrando os passageiros no vetor
+    for (var pessoa of passageiros) {
         console.log(pessoa);
-    }
+    }*/
+
+    //Teste para ver o JSON no console.log
+    var jsonPassageiros = JSON.stringify(passageiros);
+
+    console.log(jsonPassageiros);
 }
